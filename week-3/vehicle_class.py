af365401da6a5e5ac5c6e65c2648f0abc06b22d8
yibeltal marie <yibeltalmarie1228@gmail.com>
2025-02-15 23:13:03 +0300

    to show model of cars, and their description

--- week-3/vehicle_class.py
+++ week-3/vehicle_class.py
@@ -1,1 +1,25 @@
 
+
+class Vehicle:
+    def __init__(self,make,model):
+        self.make = make
+        self.model = model
+
+class Car():
+    def __init__(self,numbers_of_doors,make,model):
+        self.__numbers_of_doors = numbers_of_doors
+
+    def describe(self):
+        return "They have doors,tire,break"
+
+class Bike(Vehicle):
+    def describe (self):
+        return "They have two motor"
+
+vehicle = Vehicle(20,"2025-model")
+print(vehicle.make)
+print(vehicle.model)
+car = Car(4,2024,"model")
+print(car.describe())
+print(car._Car__numbers_of_doors)
+
