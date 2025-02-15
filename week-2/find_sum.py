524046dc38aa069ad06f8cb44efc915594d71f33
yibeltal marie <yibeltalmarie1228@gmail.com>
2025-02-15 21:47:55 +0300

    to find sum, print even numbers and largest number

--- week-2/find_sum.py
+++ week-2/find_sum.py
@@ -1,1 +1,25 @@
 
+
+def find_sum(list):
+    sum_of_elements = 0
+    for each_element in list:
+        sum_of_elements += each_element
+    return sum_of_elements
+
+list1 = [1,2,3,4,5,6]
+print(find_sum(list1))
+
+def print_even():
+    for i in range(1,21):
+        if i%2 == 0:
+            print(i)
+print_even()
+
+
+def find_maximum(list):
+    max = 0
+    for each_element in list:
+        if each_element > max:
+            max = each_element
+    return max
+print(find_maximum(list1))
